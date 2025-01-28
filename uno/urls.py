766648a0d48b8f1dcf044a77cafddfdedad1d057
1F10230238 <s1f102302382@iniad.org>
2025-01-28 11:46:39 +0900

    urlとhtmlのマッピング

--- uno/urls.py
+++ uno/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    
+    path("uno/matching", views.uno_matching, name='matching'),
+    path("uno/<str:room_name>/", views.uno_game, name="game_room")
 ]
--- uno/views.py
+++ uno/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "uno/index.html")
+
+def uno_matching(request):
+    return render(request, 'uno/matching.html')
+
+def uno_game(request, room_name):
+    return render(request, 'uno/game_room.html', {"room_name": room_name})
